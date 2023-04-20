320e4a00d5dade5f0ddf308edab586b776d99616
zyonnegb <zyonnegoodenbyrd@gmail.com>
2023-04-20 14:46:03 -0400

    Switch It Up

---
+++ switchItUp.js
@@ -0,0 +1,15 @@
+function switchItUp(n) {
+    if (n === 0) return 'Zero';
+    if (n === 1) return 'One';
+    if (n === 2) return 'Two';
+    if (n === 3) return 'Three';
+    if (n === 4) return 'Four';
+    if (n === 5) return 'Five';
+    if (n === 6) return 'Six';
+    if (n === 7) return 'Seven';
+    if (n === 8) return 'Eight';
+    if (n === 9) return 'Nine';
+  }
+  switchItUp(4);    // Four
+  switchItUp(8);    // Eight
+  switchItUp(100);  // undefined
