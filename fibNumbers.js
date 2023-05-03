f8ebd32ca730591d7a2107a0788a6c3e15e17852
zyonnegb <zyonnegoodenbyrd@gmail.com>
2023-05-03 12:27:08 -0400

    Fib Numbers

---
+++ fibNumbers.js
@@ -0,0 +1,10 @@
+function productFib(prod){
+    var n = 0;
+    var nPlus = 1;  
+    while(n*nPlus < prod) {
+      nPlus = n + nPlus;
+      n = nPlus - n;
+    }
+    return [n, nPlus, n*nPlus===prod];
+  }
+  
